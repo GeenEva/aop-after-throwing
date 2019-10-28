8bd1865077b594f16040fad14104b68f1602077b
Eva van Geenen <eva.vangeenen@incentro.com>
2019-10-28 09:54:15 +0100

    First commit

---
+++ src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,72 @@
+package com.luv2code.aopdemo.aspect;
+
+import java.util.List;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import com.luv2code.aopdemo.Account;
+
+@Aspect
+@Component
+@Order(2)
+public class MyDemoLoggingAspect {
+
+	@AfterReturning(
+			pointcut="execution(* com.luv2code.aopdemo.dao.AccountDAO.findAccounts(..))",
+			returning="result")
+	public void afterReturningFindAccountAdvice(JoinPoint theJoinPoint, List<Account> result) {
+	
+		//print out which method we're advising on
+		String method = theJoinPoint.getSignature().toShortString();
+		
+		System.out.println("\n =======>>> Executing @AfterReturnin on method: " + method);
+		
+		System.out.println("\n =======>>> The result is: " + result);
+		
+		convertAccountNamesIntoUpperCase(result);
+		
+		System.out.println("\n =======>>> The Uppercase result is: " + result);
+	}
+	
+	
+	private void convertAccountNamesIntoUpperCase(List<Account> result) {
+
+		for(Account tempAccount: result) {
+			String theUpperName = tempAccount.getName().toUpperCase();
+			tempAccount.setName(theUpperName);
+		}
+		
+	}
+
+
+	@Before("com.luv2code.aopdemo.aspect.LuvAOPExpressions.forDaoPackageNoGetterSetter()")
+	public void beforeAddAccountAdvice(JoinPoint theJoinPoint) {	
+		
+		MethodSignature methodSig = (MethodSignature) theJoinPoint.getSignature();
+		
+		System.out.println("\n=====>>> Executing @Before advice on method " + getClass() + " ORDER = 2");		
+		
+		System.out.println("\n Method: " + methodSig);
+		
+		for(Object arg: theJoinPoint.getArgs()) {
+		
+			System.out.println("\n Method arguments: " + arg);
+		
+			if(arg instanceof Account) {
+				
+				Account theAccount = (Account) arg;
+				
+				System.out.println("\n Account name: " + theAccount.getName());
+				System.out.println("\n Account level: " + theAccount.getLevel());
+				
+			}
+		}
+	}
+	
+}
